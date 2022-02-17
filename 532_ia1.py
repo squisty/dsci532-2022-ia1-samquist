bb895670461b11fda900a3815df9cd5127c111ff
Sam Quist <lummox9@gmail.com>
2022-02-16 20:57:22 -0800

    fix code ordering

--- 532_ia1.py
+++ 532_ia1.py
@@ -2,19 +2,10 @@ import altair as alt
 from dash import Dash, dcc, html, Input, Output
 from vega_datasets import data
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
 
 bar = data.barley()
-
-def plot_altair(variety):
-    chart = alt.Chart(bar[bar['variety'] < variety]).mark_point().encode(
-        x='site',
-        y='yield',
-        tooltip='variety').interactive()
-    return chart.to_html()
-
-app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
 app.layout = html.Div([
         dcc.Dropdown(
@@ -24,12 +15,20 @@ app.layout = html.Div([
             id='scatter',
             style={'border-width': '0', 'width': '100%', 'height': '400px'},
             srcDoc=plot_altair(variety='Manchuria'))])
-        
+
 @app.callback(
     Output('scatter', 'srcDoc'),
     Input('variety', 'value'))
 def update_output(xmax):
     return plot_altair(xmax)
+
+
+def plot_altair(variety):
+    chart = alt.Chart(bar[bar['variety'] < variety]).mark_point().encode(
+        x='site',
+        y='yield',
+        tooltip='variety').interactive()
+    return chart.to_html()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
